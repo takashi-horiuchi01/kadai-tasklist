8e1a7796b094b0e53bd437132ddc6b8a06a4c116
takashi.horiuchi <takashi.horiuchi01@gmail.com>
2022-01-06 20:27:53 +0000

    fix point

---
+++ db/migrate/20220106202532_tmp.rb
@@ -0,0 +1,5 @@
+class Tmp < ActiveRecord::Migration[6.1]
+  def change
+    drop_table :messages
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_06_040355) do
-
-  create_table "messages", charset: "utf8mb4", force: :cascade do |t|
-    t.string "content"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
+ActiveRecord::Schema.define(version: 2022_01_06_202532) do
 
   create_table "tasks", charset: "utf8mb4", force: :cascade do |t|
     t.string "content"
