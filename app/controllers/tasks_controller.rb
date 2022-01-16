09db18971b63e9a9c6033c6581b8c4d81f3fb415
takashi.horiuchi <takashi.horiuchi01@gmail.com>
2022-01-16 11:56:49 +0000

    add edit def

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
-  before_action :correct_user, only: [:show, :update, :destory]
+  before_action :correct_user, only: [:show, :edit, :update, :destory]
 
   def index
     @pagy, @tasks = pagy(current_user.tasks.order(id: :desc))
