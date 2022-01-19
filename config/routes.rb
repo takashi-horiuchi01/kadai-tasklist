b8d38643f3b7c98e306209b8ecb88a23a15173d9
takashi.horiuchi <takashi.horiuchi01@gmail.com>
2022-01-19 12:40:02 +0000

    del and fix

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,5 @@ Rails.application.routes.draw do
   delete 'logout', to: 'sessions#destroy'
 
   resources :tasks
-  resources :users, only: [:index, :show, :create]
+  resources :users, only: [:create]
 end
